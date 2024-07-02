6d4ebda620cd1598676e83bc6a84c32f543f50ae
pythonpro16 <117440035+pythonpro16@users.noreply.github.com>
2024-07-02 11:53:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,177 @@ from PIL import Image
 import torch
 import torch.nn as nn
 from torchvision import transforms
-import gdown
+
+import base64
+
+
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+img = get_img_as_base64("Designer - 2024-06-27T043428.404.png")
+
+page_bg_img = f"""
+<style>
+
+h1#cloud-based-plant-disease-detection-system-cbpdds {{
+    background: #f5ffc4a3;
+    font-family: "Source Sans Pro", sans-serif;
+    font-weight: 700;
+    color: rgb(14, 17, 23);
+    padding: 1.25rem 20px 2rem;
+    margin: 0px;
+    line-height: 1.2;
+    border-radius: 20px;
+    text-align: center;
+}}
+
+.st-emotion-cache-uzeiqp p {{
+    background: #f5ffc4a3;
+    font-family: "Source Sans Pro", sans-serif;
+    font-weight: 700;
+    color: rgb(14, 17, 23);
+    padding: 1.25rem 20px 2rem;
+    margin: 0px;
+    line-height: 1.2;
+    border-radius: 20px;
+    text-align: center;
+}}
+
+
+.st-emotion-cache-1vt4y43 {{
+    display: inline-flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    font-weight: 400;
+    padding: 0.25rem 0.75rem;
+    border-radius: 0.5rem;
+    min-height: 2.5rem;
+    margin: 0px;
+    line-height: 1.6;
+    color: inherit;
+    width: auto;
+    user-select: none;
+    background-color: rgb(255, 255, 255);
+    border: 1px solid rgb(100 171 9 / 82%);
+}}    
+
+/* Hover state */
+.st-emotion-cache-1vt4y43:hover {{
+    background-color: rgb(173, 255, 47); /* Yellow-Green background */
+    border: 1px solid rgb(173, 255, 47); /* Yellow-Green border */
+    color : black
+}}
+
+/* Active state */
+.st-emotion-cache-1vt4y43:active {{
+    background-color: rgb(154, 205, 50); /* Darker Yellow-Green background */
+    border: 1px solid rgb(154, 205, 50); /* Darker Yellow-Green border */
+    color : black
+}}
+
+/* visited state */
+.st-emotion-cache-1vt4y43:visited {{
+    background-color: rgb(52, 210, 235); /* Darker Yellow-Green background */
+    border: 1px solid rgb(154, 205, 50); /* Darker Yellow-Green border */
+    color : black
+}}
+
+/* focus:not(:active) state */
+.st-emotion-cache-1vt4y43:focus:not(:active) {{
+    border-color: rgb(154, 205, 50);
+    color: rgb(14, 17, 23);
+}}
+
+.st-emotion-cache-183lzff.exotz4b0 {{
+font-family: 'Poppins', monospace;
+font-size: 14px;
+overflow-x: auto;
+color: white;
+font-weight: bold;
+}}
+
+.st-emotion-cache-y4bq5x.ewgb6651 {{
+    padding: 4px;
+    background: rgb(14 17 23 / 50%);
+    border-radius: 0.5rem;
+}}
+
+.st-emotion-cache-y4bq5x {{
+    display: flex;
+    visibility: visible;
+    vertical-align: middle;
+    flex-direction: row;
+    -webkit-box-align: center;
+    align-items: center;
+    margin-top: 0.5rem;
+}}
+
+
+
+label.st-emotion-cache-ue6h4q.e1y5xkzn3 {{
+    border-radius: 0.5rem 0.5rem 0 0;
+    margin-bottom: 0px;
+    background: rgb(154 205 50 / 85%);
+    flex-direction: column;
+    color: rgb(255, 255, 255);
+}}
+
+section.st-emotion-cache-1gulkj5.e1b2p2ww15 {{
+    border-radius: 0 0 0.5rem 0.5rem;
+    background: rgb(255 255 255 / 88%);
+}}
+
+.stFileUploaderFile.st-emotion-cache-12xsiil.e1b2p2ww5 {{
+    background: rgb(240 242 246 / 77%);
+    border-radius: 0.5rem;
+    padding: 10px;
+    margin-bottom: 0;
+}}
+
+.st-emotion-cache-1v0mbdj.e115fcil1 {{
+    color: aliceblue;
+    padding: 0.5rem;
+    background: #f0f8ffe3;
+    border-radius: 0.5rem;
+}}
+
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("data:image/png;base64,{img}");
+background-size: cover;
+background-position: center;
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+
+[data-testid="stSidebar"] > div:first-child {{
+background-image: url("data:image/png;base64,{img}");
+background-position: center; 
+background-repeat: no-repeat;
+background-attachment: fixed;
+}}
+
+
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
 
 # Check device
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-
-# Google Drive file ID and URL
-file_id = '1lSun6s685Ysmytz3ij0y46aAmGR6Uwia'
-url = f'https://drive.google.com/uc?id={file_id}'
-output = 'ResNet152v2_tmodel.pt'
-
-@st.cache_data(show_spinner=False)
-def download_model():
-    gdown.download(url, output, quiet=False)
-    return output
-
-# Download the model if not already downloaded
-model_path = download_model()
-
-# Load the model
-model = torch.load(model_path, map_location=device)
+# Load the saved model weights
+model = torch.load('ResNet152v2_tmodel.pt', map_location=device)
 model.to(device)
 model.eval()
 
@@ -53,8 +203,11 @@ def predict(image):
 
 # Streamlit app
 def main():
+    # Add background image
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
     st.title("Cloud Based Plant Disease Detection System (CBPDDS)")
-    st.text("Upload an image for classification")
+    st.text("Upload a cotton plant or leaf image")
 
     # File uploader widget
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
@@ -70,9 +223,16 @@ def main():
                 probability, index = predict(uploaded_file)
                 # Display the prediction
                 if probability >= 0.99:
-                    st.write(f"Prediction: {class_names[index].capitalize()} ({probability * 100:.2f}%)")
+                    st.write(f"Prediction  :  {class_names[index].capitalize()} ({probability * 100:.2f}%)")
                 else:
-                    st.write("Prediction: None of the above")
+                     st.markdown(
+                        f'<div style="background-color: rgba(255, 0, 0, 0.8); color: white; padding: 20px;border-radius : 0.5rem;text-align : center">Error : Invalid image - Please upload a valid cotton plant or leaf image </div>',
+                        unsafe_allow_html=True)
+            
 
+                    
 if __name__ == '__main__':
     main()
+
+
+
